83e13d2566c0274bf20b6f8ede6ecf75eff7e775
Brian Bird <ProfBird@users.noreply.github.com>
2020-04-23 12:14:24 -0700

    New example, fixed typo in instructions

--- Examples/Week03/CarSeat.js
+++ Examples/Week03/CarSeat.js
@@ -2,7 +2,7 @@
 function determineSeat(age, height, weight)
 {
     let seatRequirement = "";
-    if (age >= 8 && height >= 57)
+    if (age >= 8 || height > 57)
     {
        seatRequirement = "No special seat is needed";
     }
@@ -10,7 +10,7 @@ function determineSeat(age, height, weight)
     {
         seatRequirement = "Use a booster seat";
     }
-    else if (weight >= 20 && age >= 1)
+    else if (weight >= 20 && age >= 2)
     {
         seatRequirement = "Use a front facing car seat";
     }

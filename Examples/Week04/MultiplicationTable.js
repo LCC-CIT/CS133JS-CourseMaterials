9cf963c854f0306aef56772c4bc605500bfde1fb
Brian Bird <ProfBird@users.noreply.github.com>
2020-04-29 12:11:49 -0700

    New example

---
+++ Examples/Week04/MultiplicationTable.js
@@ -0,0 +1,14 @@
+function createTable()
+{
+    let table = "";
+    for (let i = 1; i < 10;  i++)    // counts down the rows
+    {  
+        let row = "";
+        for (let j = 1; j < 10; j++)   // counts across the rows
+        {
+            row += i * j + "&emsp;";    // put a tab between each number
+        }
+       table += row + "<br>";  // add a new line
+    }
+    return table;
+}

fcbdbcfa39f51c4307c830f63b0b28d56d96729f
Brian Bird <birdb@lanecc.edu>
2020-04-30 13:40:04 -0700

    Added input validation, cleanup

--- Examples/Week04/CarSeat2.js
+++ Examples/Week04/CarSeat2.js
@@ -1,30 +1,38 @@
+// This function handles input and output for this app
 function doIO() {
-    let element = document.getElementById("seat");
-    let age = prompt("Enter your child's age.");
-    let height = prompt("Enter your child's height.");
-    let weight = prompt("Enter your child's weight.");
+    // Get input and validate it
+    let age = 0;
+    do {
+        age = prompt("Enter your child's age (from 1 to 16).");
+    } while (age < 1 || age > 16);
 
+    let height = 0;
+    do {
+        height = prompt("Enter your child's height (from 10 to 80).");
+    } while (height < 10 || height > 80);
+
+    let weight = 0;
+    do {
+        weight = prompt("Enter your child's weight (from 1 to 100).");
+    } while (weight < 1 || weight > 100);
+
+    // Determine the seating required
     let result = determineSeat(age, height, weight);
-    element.innerHTML = result;
+
+    // Put the output in the web page
+    document.getElementById("seat").innerHTML = result;
 }
 
-function determineSeat(age, height, weight)
-{
+// This function processes the input to determine the seating required for the child
+function determineSeat(age, height, weight) {
     let seatRequirement = "";
-    if (age >= 8 || height > 57)
-    {
-       seatRequirement = "no special seat";
-    }
-    else if (weight  >= 40 )
-    {
+    if (age >= 8 || height > 57) {
+        seatRequirement = "no special seat";
+    } else if (weight >= 40) {
         seatRequirement = "a booster seat";
-    }
-    else if (weight > 20 && age >= 2)
-    {
+    } else if (weight > 20 && age >= 2) {
         seatRequirement = "a front facing car seat";
-    }
-    else
-    {
+    } else {
         seatRequirement = "a rear facing car seat";
     }
     return seatRequirement;

3650efe4df199c4c6a38c600510ecbcf6b0cb819
Brian Bird <birdb@lanecc.edu>
2020-04-30 14:49:20 -0700

    Moved I/O to the .js file.

--- Examples/Week04/MultiplicationTable.js
+++ Examples/Week04/MultiplicationTable.js
@@ -1,14 +1,17 @@
-function createTable()
-{
+function createTable() {
     let table = "";
-    for (let i = 1; i < 10;  i++)    // counts down the rows
-    {  
+    for (let i = 1; i < 10; i++) // counts down the rows
+    {
         let row = "";
-        for (let j = 1; j < 10; j++)   // counts across the rows
+        for (let j = 1; j < 10; j++) // counts across the rows
         {
-            row += i * j + "&emsp;";    // put a tab between each number
+            row += i * j + "&emsp;"; // put a tab between each number
         }
-       table += row + "<br>";  // add a new line
+        table += row + "<br>"; // add a new line
     }
     return table;
+}
+
+function doIO() {
+    document.getElementById("table").innerHTML = createTable();
 }

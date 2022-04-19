f231bc3e9a09b23c8200e17dad197905b43b05ce
brian <birdb@lanecc.edu>
2022-04-18 17:03:23 -0700

    New lab 4, part 1

---
+++ Labs/Lab04/CS133JS_Lab04_Instructions-Part2.md
@@ -0,0 +1,85 @@
+<h1>Lab 4, Part 2 Instructions</h1>
+
+**CS 133JS, Beginning Programming: JavaScript**
+
+## Overview
+
+The objective of this lab is to give you practice using:  while loops, do-while loops, for loops and nested loops.
+
+## Web Apps
+
+For each of these pages:
+
+- Put the code that does processing or calculations in a function in a .js file.
+- Put I/O operations in a separate function in the .js file. I/O operations include:
+  - prompt
+  - document.getElementById
+  - document.write 
+- In your .html files, use `onload="someIOFunction()"` to call your I/O function after the page has loaded.
+
+ 
+
+1. New Year’s Eve Countdown 
+   Enter number of hours until midnight, count down the hours and show minutes too.
+   Example output if the user entered 4:
+   Hours, Minutes
+   4,      240
+   3,      180
+   2,      120
+   1,      60
+   0,       0
+
+2. Kindergarten Admission Age
+
+Add an input validation loop to web app II from the previous lab. 
+
+·    Limit user input to reasonable values. 
+
+·    Pop up an alert telling the user what the acceptable input range is.
+
+·    Keep prompting the user for input until it is valid.
+
+ 
+
+III. Elementary School Grade Level
+
+Modify your web app III from the last lab by:
+
+\1.   Adding an input validation loop as you did for web app II above.
+
+\2.   Adding a loop that lets the user enter another age. After the results have been shown on the page, ask the user if they want to run the program again.
+
+ 
+
+**Submitting your lab work on Moodle**
+
+Beta Version
+
+Post the following in the *Lab Beta forum*:
+
+\1.   The web pages you created for part 2.
+ (Zip the files for you web pages and attach them to the post.)
+
+\2.   A code review of your lab partner’s web page for part 2. 
+ (Review one of your lab partners’ web pages using the Code Review Form provided.)
+
+ 
+
+Code Review
+
+\1.   Submit a copy of the code review above to the *Lab Code Review assignment*.
+
+
+Production Version
+ Based on the review and helpful advice from your lab partner, you may revise your web page. On the code review from your lab partner, complete the “Production” column to show what you revised. Upload the following to the *Lab Production Version* assignment:
+
+\1.   A Word document containing all the code you ran for part 1.
+
+\2.   The web pages you created for part 2.
+
+\3.   The code review from your lab partner with the “Prod” column filled in by you.
+
+[^]: 
+[^1]: Methods are pretty much the same as functions, but you call them using the name of some *object*, like a string variable, with a dot and then the name of of the method and any arguments in parenthesis. For example: `someText.charAt(0)`
+[^2]: The characters in a string are counted with the first character being at position 0. For example, the 'T', in this string is at position 0: `let someText = "The only thing constant in life is change.";` If we want to get the 6th character, 'n', we would execute the statement: `let character = someText.charAt(5);`
+
--- Labs/Lab04/CS133JS_Lab04_Part1_Starter.js
+++ Labs/Lab04/CS133JS_Lab04_Part1_Starter.js
@@ -2,10 +2,90 @@
  * Lab 4, Part 1: Exercise starter file by Brian Bird, spring 2022  *
  * For each of the problems, complete the function.                 */
 
-// 1. Use a while loop to count the number of words in a string of text.
-function countWords(text)
-{
-    let wordCount = 0;
-    
+// 1. Complete the do while loop below so that it counts from 2 to 10 by twos (2, 4, 6, 8)
+function doWhilePractice() {
+    let outputString = "";
+    let count = 0; // TODO: Set count to the right starting value
+    do {
+        // TODO: Add two to count
+        outputString += count;
+    } while (count < 0); // TODO: put in the correct condition
+    return outputString;
+}
+
+// 2. Complete the while loop below so that it counts from 2 to 10 by twos (2, 4, 6, 8)
+function whilePractice() {
+    let outputString = "";
+    let count = 0; // TODO: Set count to the right starting value
+    while (count < 0) // TODO: put in the correct condition
+    {
+        // TODO: Add two to count
+        outputString += count;
+    }
+    return outputString;
+}
+
+// 3. Complete the for loop below so that it counts from 2 to 10 by twos (2, 4, 6, 8)
+function forPractice() {
+    let outputString = "";
+    // TODO: Set the count staring value, the loop condition, and incrment expression
+    for (count = 0; count < 0; count += 0) {
+        outputString += count;
+    }
+    return outputString;
+}
+
+// 4. Use a do while loop to count the number of characters in a sentence.
+function countChars(text) {
+    let index = 0; // Beginning position of the string
+    // TODO: Write the code to complete the exercise.
+    return index;
+}
+
+// 5. Use a while loop to count the number of words in a sentence.
+function countWords(text) {
+    let wordCount = 1; // There should always be at least one word
+    // TODO: Write the code to complete the exercise.
     return wordCount;
+}
+
+// 6. Use a while loop to check a number to see if it is prime.
+function isPrime(maybePrime) {
+    let isPrimeNumber = false;
+    // TODO: Write the code to complete the exercise.
+    return isPrimeNumber;
+}
+
+// 7. Use a do while loop to find the whole square root of a number
+function squareRoot(number) {
+    let root = 1; // This is just a starting point
+    // TODO: Write the code to complete the exercise.
+    return root;
+}
+
+// 8. Use a for loop to raise a number to a power
+function power(base, exponent) {
+    let result = 1;
+    // TODO: Write the code to complete the exercise.
+    return result;
+}
+
+// 9. Use a for loop to check a number to see if it is prime.
+function isPrime2(maybePrime) {
+    let isPrimeNumber = true;
+    // TODO: Write the code to complete the exercise.
+    return isPrimeNumber;
+}
+
+// This function is used in the web page to check the other functions
+function checkAnswer(elementId, correctAnswer, answer) {
+    const element = document.getElementById(elementId);
+    element.innerHTML += answer;
+    if (correctAnswer === answer) {
+        element.innerHTML += ", correct";
+        element.style.backgroundColor = "green";
+    } else {
+        element.innerHTML += ", incorrect";
+        element.style.backgroundColor = "red";
+    }
 }

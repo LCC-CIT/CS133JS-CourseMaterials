fd7c495193ac09eed9ec707dd256d2762c99f0ae
brian <birdb@lanecc.edu>
2022-05-11 17:54:16 -0700

    Finished refactoring GpaCalculator to be more OO.

--- Examples/Week07/GpaCalculator-Object.js
+++ Examples/Week07/GpaCalculator-Object.js
@@ -1,74 +1,70 @@
-/* Written by Brian Bird, 5/16/2020 */
+/* Written by Brian Bird, 5/16/2020 *
+ * Revised by Brian Bird, 5/11/2022 */
 
 "use strict";
 
 // Object constructor for a course object
 function Course(courseNumber, credits, grade) {
+    // Individual course properties
     this.number = courseNumber; // A string
-    this.credits = credits; // Creidts for this course
-    this.grade = grade; // Grade earned
-}
+    this.credits = credits;
+    this.grade = grade;
 
-const gpaCalculator = {
-        courses: [], // array to hold course objects
+    // Lookup table for converting a letter grade to grade points
+    this.pointLookup = [
+        ["A+", 4.3],
+        ["A", 4.0],
+        ["A-", 3.7],
+        ["B+", 3.3],
+        ["B", 3.0],
+        ["B-", 2.7],
+        ["C+", 2.3],
+        ["C", 2.0],
+        ["C-", 1.7],
+        ["D+", 1.3],
+        ["D", 1.0],
+        ["D-", 0.7],
+        ["F", 0.0]
+    ];
 
-    }
-    // A global array that will hold course objects
-let courses = [];
-
-// Looup table for converting letter grades to grade points
-// Each nested array has two elements:
-// Index 0: the letter grade
-// Index 1: the points for that grade
-let pointLookup = [
-    ["A+", 4.3],
-    ["A", 4.0],
-    ["A-", 3.7],
-    ["B+", 3.3],
-    ["B", 3.0],
-    ["B-", 2.7],
-    ["C+", 2.3],
-    ["C", 2.0],
-    ["C-", 1.7],
-    ["D+", 1.3],
-    ["D", 1.0],
-    ["D-", 0.7],
-    ["F", 0.0]
-];
-
-// Convert a letter grade to a grade point
-function lookUpPoints(grade) {
-    let points = -1; // if this gets returned, then the grade wasn't found
-    // Loop until either the grade is found or we reach the end of the array
-    for (let i = 0; i < pointLookup.length && points < 0; i++) {
-        if (grade == pointLookup[i][0]) {
-            points = pointLookup[i][1]; // set points equal to the value in the 2nd element of the nested array.
+    // Convert a letter grade to a grade point
+    this.getPoints = function() {
+        let points = -1; // return -1 if the grade isn't found
+        // Find the grade and get the points
+        for (let i = 0; i < this.pointLookup.length && points < 0; i++) {
+            if (this.grade == this.pointLookup[i][0]) {
+                points = this.pointLookup[i][1]; // set points equal to the value in the 2nd element of the nested array.
+            }
         }
+        return points;
     }
-    return points;
 }
 
-// Add a class. This requires adding to all three global arrays
-function addClass(name, credits, grade) {
-    courses.push(new Course(name, credits, grade));
-}
+// gpaCalculator object literal
+const gpaCalculator = {
+    // array to hold course objects
+    courses: [],
 
-// Add up the total of the credits using the global courseCredits array
-function totalCredits() {
-    let total = 0;
-    for (let course of courses) {
-        total += course.credits;
-    }
-    return total;
-}
+    // Add a course to the array
+    addCourse: function(name, credits, grade) {
+        this.courses.push(new Course(name, credits, grade));
+    },
 
-// Calculate GPA, based on quality points, using the global courses array
-// For each class, quality points = grade point * credits
-// For the overall grade point average, GPA = total quality points / number of credits
-function calcGpa() {
-    let points = 0;
-    for (let i = 0; i < courses.length; i++) {
-        points += lookUpPoints(courses[i].grade) * courses[i].credits;
+    // Add up the total of the credits for all courses
+    totalCredits: function() {
+        let total = 0;
+        for (let course of this.courses) {
+            total += course.credits;
+        }
+        return total;
+    },
+
+    // Calculate GPA, based on quality points
+    calcGpa: function() {
+        let points = 0;
+        for (let i = 0; i < this.courses.length; i++) {
+            points += this.courses[i].getPoints() * this.courses[i].credits;
+        }
+        return points / this.totalCredits();
     }
-    return points / totalCredits();
 }

83cd8de664285e8141c765ae8a5983e0ccca7b0f
brian <birdb@lanecc.edu>
2022-05-21 13:05:23 -0700

    New chatbot2 project and revisions

---
+++ Examples/Week09/ChatBot2/README.md
@@ -0,0 +1,11 @@
+# JavaScript Chatbot 
+
+This is a very simple "chatbot" using nothing but JavaScript, HTML, & CSS. I put this in quotes because chatbots these days are way more complex - this is an exercise in vanilla JS loops and conditionals, not AI.
+
+I hope it makes a good intro for anyone interested in chatbots and frontend fundamentals.
+
+Enjoy!
+
+[Demo](https://sylviapap.github.io/chatbot/)
+
+[Blog post](https://dev.to/sylviapap/make-a-simple-chatbot-with-javascript-1gc)
---
+++ Examples/Week09/ChatBot2/replies.js
@@ -0,0 +1,37 @@
+// Possible responses, in order, corresponding to prompts
+const replies = [
+    ["Hello!", "Hi!", "Hey!", "Hi there!", "Howdy"],
+    ["Fine... how are you?", "Pretty well, how are you?", "Fantastic, how are you?"],
+    ["Nothing much", "About to go to sleep", "Can you guess?", "I don't know actually"],
+    ["I am infinite"],
+    ["I am just a bot", "I am a bot. What are you?"],
+    ["The one true God, JavaScript"],
+    ["I am nameless", "I don't have a name"],
+    ["I love you too", "Me too"],
+    ["Have you ever felt bad?", "Glad to hear it"],
+    ["Why?", "Why? You shouldn't!", "Try watching TV"],
+    ["What about?", "Once upon a time..."],
+    ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+    ["Bye", "Goodbye", "See you later"],
+    ["Sushi", "Pizza"],
+    ["Bro!"],
+    ["Great question"],
+    ["That's ok", "I understand", "What do you want to talk about?"],
+    ["Please say something :("],
+    ["Haha!", "Good one!"]
+]
+
+// Random for any other user input
+
+const alternative = [
+    "Same",
+    "Go on...",
+    "Bro...",
+    "Try again",
+    "I'm listening...",
+    "I don't understand :/"
+]
+
+// Whatever else you want :)
+
+const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
